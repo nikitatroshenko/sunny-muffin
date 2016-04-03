3941fac4197e2b127e028e4211e02f1b7158e73c
VenskiyIlya <vensilya@gmail.com>
2016-04-03 23:51:41 +0300

    add auth function

--- Operator.h
+++ Operator.h
@@ -9,3 +9,7 @@ void debit(sqlite3 * db, char* sql, char* zErrMsg, int, int);
 void credit(sqlite3 * db, char* sql, char* zErrMsg, int, int);
 
 void transfer(sqlite3 * db, char* sql, char* zErrMsg, int, int, int);
+
+int checkPassword(sqlite3 * db, char* sql, char* zErrMsg, char *username, char *password);
+
+int authentication(sqlite3 * db, char* sql, char* zErrMsg, char *username, char *password);
