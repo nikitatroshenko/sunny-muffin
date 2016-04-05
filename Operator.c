336eb88eef6e8e767d4a310b06fa88e999991aad
Ilya <vensilya@gmail.com>
2016-04-05 22:59:58 +0300

    Added files via upload

--- Operator.c
+++ Operator.c
@@ -1,19 +1,18 @@
-#include "Operator.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sqlite3.h>
+#include "sqlite3.h"
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
-    int i;
-    for(i=0; i<argc; i++){
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-    }
-    printf("\n");
-    return 0;
+	int i;
+	for (i = 0; i<argc; i++){
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	printf("\n");
+	return 0;
 }
 
-void balanceChange(sqlite3 * db, char* sql, char* zErrMsg, int accountNumber, int money)
+void balanceChange(sqlite3 * db, char* zErrMsg, int accountNumber, int money)
 {
 	int balance, newBalance;
 	int rc;
@@ -21,26 +20,27 @@ void balanceChange(sqlite3 * db, char* sql, char* zErrMsg, int accountNumber, in
 
 	char str[300];
 	sprintf(str, "SELECT balance FROM BANK_ACCOUNTS WHERE account_number = %d", accountNumber);
-        
-    rc = sqlite3_prepare_v2(db, str, -1, &res, 0);
-    
-    if (rc == SQLITE_OK) {       
-        sqlite3_bind_int(res, 1, 3);
-    } else {
-        
-        fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
-    }
-    
-    int step = sqlite3_step(res);
-    
-    if (step == SQLITE_ROW) {
-        
-        balance = sqlite3_column_int(res, 0);    
-    } 
 
-    newBalance = balance + money;
-    if(newBalance >= 0)
-    {
+	rc = sqlite3_prepare_v2(db, str, -1, &res, 0);
+
+	if (rc == SQLITE_OK) {
+		sqlite3_bind_int(res, 1, 3);
+	}
+	else {
+
+		fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
+	}
+
+	int step = sqlite3_step(res);
+
+	if (step == SQLITE_ROW) {
+
+		balance = sqlite3_column_int(res, 0);
+	}
+
+	newBalance = balance + money;
+	if (newBalance >= 0)
+	{
 		char strUpdate[300];
 		sprintf(strUpdate, "UPDATE BANK_ACCOUNTS SET balance = %d WHERE account_number = %d;", newBalance, accountNumber);
 
@@ -48,40 +48,40 @@ void balanceChange(sqlite3 * db, char* sql, char* zErrMsg, int accountNumber, in
 
 		if (rc != SQLITE_OK)
 		{
-		    printf ("ERROR: %s\n", zErrMsg);
-		    sqlite3_free(zErrMsg);
-		    
+			printf("ERROR: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+
 		}
 		else
 		{
-		    printf ("\nSuccess!\n");
-		    
+			printf("\nSuccess!\n");
+
 		}
 	}
 	else
 		printf("Failed\n");
 
-    sqlite3_finalize(res);
+	sqlite3_finalize(res);
 }
 
-void debit(sqlite3 * db, char* sql, char* zErrMsg, int accountNumber, int moneyDebit)
+void debit(sqlite3 * db, char* zErrMsg, int accountNumber, int moneyDebit)
 {
-	balanceChange(db, sql, zErrMsg, accountNumber, -moneyDebit);
+	balanceChange(db, zErrMsg, accountNumber, -moneyDebit);
 }
 
-void credit(sqlite3 * db, char* sql, char* zErrMsg, int accountNumber, int moneyCredit)
+void credit(sqlite3 * db, char* zErrMsg, int accountNumber, int moneyCredit)
 {
-	balanceChange(db, sql, zErrMsg, accountNumber, moneyCredit);
+	balanceChange(db, zErrMsg, accountNumber, moneyCredit);
 }
 
-void transfer(sqlite3 * db, char* sql, char* zErrMsg, int firstUser, int secondUser, int moneyTransfer)
+void transfer(sqlite3 * db, char* zErrMsg, int firstUser, int secondUser, int moneyTransfer)
 {
-	balanceChange(db, sql, zErrMsg, firstUser, -moneyTransfer);
+	balanceChange(db, zErrMsg, firstUser, -moneyTransfer);
 
-	balanceChange(db, sql, zErrMsg, secondUser, moneyTransfer);
+	balanceChange(db, zErrMsg, secondUser, moneyTransfer);
 }
 
-int checkPassword(sqlite3 * db, char* sql, char* zErrMsg, char *username, char *password)
+int checkPassword(sqlite3 * db, char* zErrMsg, char *username, char *password)
 {
 	int rc;
 	sqlite3_stmt *res;
@@ -90,27 +90,37 @@ int checkPassword(sqlite3 * db, char* sql, char* zErrMsg, char *username, char *
 	sprintf(str, "SELECT password FROM BANK_USERS WHERE username = \"%s\";", username);
 
 	rc = sqlite3_prepare_v2(db, str, -1, &res, 0);
-    
-    if (rc == SQLITE_OK) {       
-        sqlite3_bind_int(res, 1, 3);
-    } else {
-        
-        fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
-    }
-    
-    int step = sqlite3_step(res);
- 
-    if (step == SQLITE_ROW) {
-    	if(!strcmp(password, sqlite3_column_text(res, 0)))
-    		return 1;
-    	else
-    		return 0;
-    }
-    sqlite3_finalize(res);
+
+	if (rc == SQLITE_OK) {
+		sqlite3_bind_int(res, 1, 3);
+	}
+	else 
+	{
+		fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
+		sqlite3_finalize(res);
+		return 0;
+	}
+
+	int step = sqlite3_step(res);
+
+	if (step == SQLITE_ROW) {
+		if (!strcmp(password, sqlite3_column_text(res, 0)))
+		{
+			sqlite3_finalize(res);
+			return 1;
+		}
+		else
+		{
+			sqlite3_finalize(res);
+			return 0;
+		}
+	}
+	sqlite3_finalize(res);
+	return 0;
 }
 
 
-int authentication(sqlite3 * db, char* sql, char* zErrMsg, char *username, char *password)
+int authentication(sqlite3 * db, char* zErrMsg, char *username, char *password)
 {
 	int rc;
 	sqlite3_stmt *res;
@@ -119,43 +129,43 @@ int authentication(sqlite3 * db, char* sql, char* zErrMsg, char *username, char 
 	sprintf(str, "SELECT username FROM BANK_USERS WHERE username = \"%s\";", username);
 
 	rc = sqlite3_prepare_v2(db, str, -1, &res, 0);
-    
-    if (rc == SQLITE_OK) {       
-        sqlite3_bind_int(res, 1, 3);
-    } else {
-        fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
-    }
-    
-    int step = sqlite3_step(res);
- 
-    if (step == SQLITE_ROW)
-    {
-		return checkPassword(db, sql, zErrMsg, username, password);
-    }
-    else
-    {
-    	return 0; 
-    }
 
-    sqlite3_finalize(res);
+	if (rc == SQLITE_OK) {
+		sqlite3_bind_int(res, 1, 3);
+	}
+	else {
+		fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
+	}
+
+	int step = sqlite3_step(res);
+
+	if (step == SQLITE_ROW)
+	{
+		return checkPassword(db, zErrMsg, username, password);
+	}
+	else
+	{
+		return 0;
+	}
+
+	sqlite3_finalize(res);
 }
 
 int main(int argc, const char *argv[])
 {
 	sqlite3 *db;
-    if (sqlite3_open("Bank_System_Database", &db) == SQLITE_OK)
-        printf("Opened db successfully\n");
-    else
-        printf("Failed to open db\n");
+	if (sqlite3_open("Bank_System_Database", &db) == SQLITE_OK)
+		printf("Opened db successfully\n");
+	else
+		printf("Failed to open db\n");
 
 	char *zErrMsg = 0;
 	int rc = 0;
-	char* sql;
 
 	// debit(db, sql, zErrMsg, 0, 50);
 	// credit(db, sql, zErrMsg, 0, 50);
 	// transfer(db, sql, zErrMsg, 0, 1, 50);
-	if(authentication(db, sql, zErrMsg, "Venskiy", "1234"))
+	if (authentication(db, zErrMsg, "Venskiy", "1234"))
 		printf("success\n");
 	else
 		printf("fail\n");
